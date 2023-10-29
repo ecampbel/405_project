cac599b18f8cfc0443e31af120f6e4a18f5b9c7c
iporter-16 <63209879+iporter-16@users.noreply.github.com>
2023-10-29 14:12:36 -0700

    Imogen work

--- gene_function_annotation.R
+++ gene_function_annotation.R
@@ -4,17 +4,29 @@ library(dplyr)
 
 #Goal: add a column to the read data with gene function
 gene_function_file <- "uniprotkb_taxonomy_id_188477_2023_10_29.tsv"
-gene_function_info <- read.delim(gene_function_file,header=TRUE,sep="\t")
+gene_function_info <- read.delim(gene_function_file,header=TRUE,sep="\t") 
+gene_function_info <- gene_function_info[order(gene_function_info$Gene.Names),]
+gene_function_info <- gene_function_info["EGW08"%in%gene_function_info$Gene.Names==TRUE,]
 
 read_files <- c("10d1ReadsPerGene.out.tab","10d2ReadsPerGene.out.tab","10d3ReadsPerGene.out.tab",
                 "apo1ReadsPerGene.out.tab","apo2ReadsPerGene.out.tab","apo3ReadsPerGene.out.tab")
 
-file <- read.delim("10d1ReadsPerGene.out.tab",header=FALSE,sep="\t")
-file <- file %>% tail(-4), colnames()
+file <- read.delim("10d1ReadsPerGene.out.tab",header=FALSE,sep="\t") %>% tail(-4) 
+colnames(file) <-  c("Gene.Names","exp1","exp2","exp3")
 
-for (rf in read_files) {annotate(r)}
+for (rf in read_files) {main(r)}
 
-annotate <- function(read_file){
-  file <- read.delim(read_file)
+main <- function(read_file){
+  file <- read.delim(read_file,header=FALSE,sep="\t") %>% tail(-4) 
+  colnames(file) <-  c("Gene.Names","exp1","exp2","exp3")
+  file_annotated <- annotate(file)
+  write_it(file)
+}
+
+annotate <- function(file){
+  
+}
+
+write_it <- function(file){
   
 }
