524a242ccbdbb98b4747c97023cb4a55c5d61ff1
iporter-16 <63209879+iporter-16@users.noreply.github.com>
2023-10-29 14:20:57 -0700

    More changes

--- gene_function_annotation.R
+++ gene_function_annotation.R
@@ -6,7 +6,6 @@ library(dplyr)
 gene_function_file <- "uniprotkb_taxonomy_id_188477_2023_10_29.tsv"
 gene_function_info <- read.delim(gene_function_file,header=TRUE,sep="\t") 
 gene_function_info <- gene_function_info[order(gene_function_info$Gene.Names),]
-gene_function_info <- gene_function_info["EGW08"%in%gene_function_info$Gene.Names==TRUE,]
 
 read_files <- c("10d1ReadsPerGene.out.tab","10d2ReadsPerGene.out.tab","10d3ReadsPerGene.out.tab",
                 "apo1ReadsPerGene.out.tab","apo2ReadsPerGene.out.tab","apo3ReadsPerGene.out.tab")
@@ -16,17 +15,14 @@ colnames(file) <-  c("Gene.Names","exp1","exp2","exp3")
 
 for (rf in read_files) {main(r)}
 
-main <- function(read_file){
-  file <- read.delim(read_file,header=FALSE,sep="\t") %>% tail(-4) 
-  colnames(file) <-  c("Gene.Names","exp1","exp2","exp3")
-  file_annotated <- annotate(file)
-  write_it(file)
+main <- function(rf){
+  file <- read.delim(rf,header=FALSE,sep="\t") %>% tail(-4) 
+  colnames(file) <-  c("Gene.Names","Rep1","Rep2","Rep3")
+  file_annotated <- left_join(file,gene_function_info,by=c("Gene.Name"))
+  write_it(file,rf)
 }
 
-annotate <- function(file){
-  
-}
-
-write_it <- function(file){
-  
+write_it <- function(annotated,rf){
+  filename <- paste(rf,".annotated",sep="")
+  write(annotated, file=filename,sep="\t")
 }
