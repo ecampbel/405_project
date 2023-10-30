180f93f61517f54ceaf846a8514edc61f3e32d07
iporter-16 <63209879+iporter-16@users.noreply.github.com>
2023-10-29 21:15:53 -0700

    Tested QC analysis -imogen

--- R_scripts/gene_function_annotation.R
+++ R_scripts/gene_function_annotation.R
@@ -1,6 +1,9 @@
 #/bin/envr/R!
+# install.packages("reshape")
 library(tidyr)
 library(dplyr)
+library(ggplot2)
+library(reshape2)
 
 #Read gene function data
 gene_function_file <- "uniprotkb_taxonomy_id_188477_2023_10_29.tsv"
@@ -31,17 +34,34 @@ read_it <- function(rf){
   file <- read.delim(rf,header=FALSE,sep="\t") %>% tail(-4) 
   colnames(file) <-  c("geneID",paste(substring(rf,1,4),"Rep1",sep="_"),paste(substring(rf,1,4),"Rep2",sep="_"),paste(substring(rf,1,4),"Rep3",sep="_"))
   return(file)
-  }
+}
 
 #Writes annotated files (with a name you provide)
 write_it <- function(annotated,name){
   filename <- paste(name,".annotated.tsv",sep="")
   print(filename)
   write.table(annotated, file=filename,sep="\t",row.names=FALSE,col.names=TRUE)
-  }
+}
 
 ##### ANNOTATION OF NORMALISED FILE
 normalised <- read.csv("echlorotica_TPM.csv") 
 names(gene_function_info)[names(gene_function_info) == 'Gene.Names'] <- 'geneID'
 normalised_annotated <- left_join(normalised,gene_function_info,by=c("geneID"))
 write_it(normalised_annotated,"normalised_annotated")
+
+###### Imogen messes around ################################################################################################
+analysis <- matrix(nrow=nrow(normalised_annotated),ncol=10)
+for (n in 1:6){analysis[,n] <- normalised_annotated[,n+2]}
+for (n in 1:nrow(analysis)){analysis[n,7] <- mean(analysis[n,1:3])}  #d10 means
+for (n in 1:nrow(analysis)){analysis[n,8] <- sd(analysis[n,1:3])}   #d10 stdev
+for (n in 1:nrow(analysis)){analysis[n,9] <- mean(analysis[n,4:6])}  #apo means
+for (n in 1:nrow(analysis)){analysis[n,10] <- sd(analysis[n,4:6])}  #apo stdev
+analysis_df <- as.data.frame(analysis) 
+colnames(analysis_df) <- c("t10R1","t10R2","t10R3","apoR1","apoR2","apoR3","t10_av","t10_sd","apo_av","apo_sd")
+
+analysis_df2 <- as.data.frame(matrix(nrow=nrow(normalised_annotated),ncol=6)) 
+for (n in 1:6){analysis_df2[,n] <- normalised_annotated[,n+2]}
+colnames(analysis_df2) <- c("t10R1","t10R2","t10R3","apoR1","apoR2","apoR3")
+analysis_df2 <- melt(analysis_df2)
+ggplot(analysis_df2,mapping=aes(variable,value))+geom_boxplot()+scale_y_log10() #recreates gene count 
+ggplot(analysis_df2,mapping=aes(value))+geom_histogram()+scale_x_log10() #as histogram, logarithmic
