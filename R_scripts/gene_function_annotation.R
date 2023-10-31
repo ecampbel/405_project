3e4cc58fa433b426da3404413863e0bf4c52e8e6
iporter-16 <63209879+iporter-16@users.noreply.github.com>
2023-10-31 12:16:29 -0700

    Imogen - just trying things out

--- R_scripts/gene_function_annotation.R
+++ R_scripts/gene_function_annotation.R
@@ -4,18 +4,20 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(reshape2)
+library(gplots)
+library(edgeR)
 
 #Read gene function data
-gene_function_file <- "uniprotkb_taxonomy_id_188477_2023_10_29.tsv"
+gene_function_file <- "../uniprotkb_taxonomy_id_188477_2023_10_29.tsv"
 gene_function_info <- read.delim(gene_function_file,header=TRUE,sep="\t") 
 gene_function_info <- gene_function_info[order(gene_function_info$Gene.Names),]
 
-rf10d1 <- "10d1ReadsPerGene.out.tab"
-rf10d2 <- "10d2ReadsPerGene.out.tab"
-rf10d3 <- "10d3ReadsPerGene.out.tab"
-apod1 <- "apo1ReadsPerGene.out.tab"
-apod2 <- "apo2ReadsPerGene.out.tab"
-apod3 <- "apo3ReadsPerGene.out.tab"
+rf10d1 <- "../rawCounts/10d1ReadsPerGene.out.tab"
+rf10d2 <- "../rawCounts/10d2ReadsPerGene.out.tab"
+rf10d3 <- "../rawCounts/10d3ReadsPerGene.out.tab"
+apod1 <- "../rawCounts/apo1ReadsPerGene.out.tab"
+apod2 <- "../rawCounts/apo2ReadsPerGene.out.tab"
+apod3 <- "../rawCounts/apo3ReadsPerGene.out.tab"
 
 #Read and join our ReadsPerOut files
 merged_10d <- merge(read_it(rf10d1),read_it(rf10d2),by=c("geneID")) %>% merge(read_it(rf10d3),by=c("geneID"))
@@ -44,24 +46,10 @@ write_it <- function(annotated,name){
 }
 
 ##### ANNOTATION OF NORMALISED FILE
-normalised <- read.csv("echlorotica_TPM.csv") 
+normalised <- read.csv("../echlorotica_TPM.csv") 
 names(gene_function_info)[names(gene_function_info) == 'Gene.Names'] <- 'geneID'
 normalised_annotated <- left_join(normalised,gene_function_info,by=c("geneID"))
-write_it(normalised_annotated,"normalised_annotated")
+# write_it(normalised_annotated,"normalised_annotated")
 
-###### Imogen messes around ################################################################################################
-analysis <- matrix(nrow=nrow(normalised_annotated),ncol=10)
-for (n in 1:6){analysis[,n] <- normalised_annotated[,n+2]}
-for (n in 1:nrow(analysis)){analysis[n,7] <- mean(analysis[n,1:3])}  #d10 means
-for (n in 1:nrow(analysis)){analysis[n,8] <- sd(analysis[n,1:3])}   #d10 stdev
-for (n in 1:nrow(analysis)){analysis[n,9] <- mean(analysis[n,4:6])}  #apo means
-for (n in 1:nrow(analysis)){analysis[n,10] <- sd(analysis[n,4:6])}  #apo stdev
-analysis_df <- as.data.frame(analysis) 
-colnames(analysis_df) <- c("t10R1","t10R2","t10R3","apoR1","apoR2","apoR3","t10_av","t10_sd","apo_av","apo_sd")
 
-analysis_df2 <- as.data.frame(matrix(nrow=nrow(normalised_annotated),ncol=6)) 
-for (n in 1:6){analysis_df2[,n] <- normalised_annotated[,n+2]}
-colnames(analysis_df2) <- c("t10R1","t10R2","t10R3","apoR1","apoR2","apoR3")
-analysis_df2 <- melt(analysis_df2)
-ggplot(analysis_df2,mapping=aes(variable,value))+geom_boxplot()+scale_y_log10() #recreates gene count 
-ggplot(analysis_df2,mapping=aes(value))+geom_histogram()+scale_x_log10() #as histogram, logarithmic
+
