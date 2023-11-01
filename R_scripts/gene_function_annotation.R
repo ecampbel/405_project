c58686b249b533e612a7d6fbcf09e16275992d47
iporter-16 <63209879+iporter-16@users.noreply.github.com>
2023-11-01 16:39:52 -0700

    Improved heatmaps -i

--- R_scripts/gene_function_annotation.R
+++ R_scripts/gene_function_annotation.R
@@ -12,6 +12,18 @@ gene_function_file <- "../uniprotkb_taxonomy_id_188477_2023_10_29.tsv"
 gene_function_info <- read.delim(gene_function_file,header=TRUE,sep="\t") 
 gene_function_info <- gene_function_info[order(gene_function_info$Gene.Names),]
 
+#Helpers
+#Reads a ReadsPerGene.out.tab file, renames its columns.
+read_it <- function(rf){
+  file <- read.delim(rf,header=FALSE,sep="\t") %>% tail(-4) 
+  colnames(file) <-  c("geneID",paste(substring(rf,1,4),"Rep1",sep="_"),paste(substring(rf,1,4),"Rep2",sep="_"),paste(substring(rf,1,4),"Rep3",sep="_"))
+  return(file)}
+#Writes annotated files (with a name you provide)
+write_it <- function(annotated,name){
+  filename <- paste(name,".annotated.tsv",sep="")
+  print(filename)
+  write.table(annotated, file=filename,sep="\t",row.names=FALSE,col.names=TRUE)}
+
 rf10d1 <- "../rawCounts/10d1ReadsPerGene.out.tab"
 rf10d2 <- "../rawCounts/10d2ReadsPerGene.out.tab"
 rf10d3 <- "../rawCounts/10d3ReadsPerGene.out.tab"
@@ -27,22 +39,6 @@ merged_apo <- merge(read_it(apod1),read_it(apod2),by=c("geneID")) %>% merge(read
 main <- function(merged_file,name){
   file_annotated <- left_join(merged_file,gene_function_info,by=c("geneID"))
   write_it(file_annotated,name)
-}
-
-### HELPER FUNCTIONS #####
-
-#Reads a ReadsPerGene.out.tab file, renames its columns.
-read_it <- function(rf){
-  file <- read.delim(rf,header=FALSE,sep="\t") %>% tail(-4) 
-  colnames(file) <-  c("geneID",paste(substring(rf,1,4),"Rep1",sep="_"),paste(substring(rf,1,4),"Rep2",sep="_"),paste(substring(rf,1,4),"Rep3",sep="_"))
-  return(file)
-}
-
-#Writes annotated files (with a name you provide)
-write_it <- function(annotated,name){
-  filename <- paste(name,".annotated.tsv",sep="")
-  print(filename)
-  write.table(annotated, file=filename,sep="\t",row.names=FALSE,col.names=TRUE)
 }
 
 ##### ANNOTATION OF NORMALISED FILE
