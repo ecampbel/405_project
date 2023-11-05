1ed6f05410c104fa756845fd826a5353e5c8b767
iporter-16 <63209879+iporter-16@users.noreply.github.com>
2023-11-04 19:13:19 -0700

    Filtering for significantly altered pathways

--- R_scripts/imogen_DESeq.R
+++ R_scripts/imogen_DESeq.R
@@ -18,6 +18,14 @@ res <- results(dds, name="Condition_d10_vs_apo")
 resLFC <- lfcShrink(dds, coef="Condition_d10_vs_apo", type="apeglm")
 resOrdered <- na.omit(res[order(res$pvalue),])
 
+### Filter for significance
+sig <- na.omit(res[na.omit(res$padj)<0.05,])
+sig_up <- as.data.frame(sig[sig$log2FoldChange>0,])
+sig_down <- sig[sig$log2FoldChange<0,]
+go_data <- gene_function_info[,c(4,8)]
+sig_up_annotated <- left_join(as.data.frame(rownames_to_column(sig_up)),go_data,by=c("rowname"="Gene.Names"))
+sig_up_annotated <- sig_up_annotated[sig_up_annotated$Gene.Ontology.IDs!="",]
+
 ### Get summary data
 summary(res)
 
