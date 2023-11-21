260bfb00e3371a6ac0aeb886ea3bf10ef551a333
Eleanor Campbell <ecampbell@dhcp-206-12-42-211.ubcsecure.wireless.ubc.ca>
2023-11-21 12:41:29 -0800

    updated top go

--- eChloroticaR_Directory/topGoAnalysis.R
+++ eChloroticaR_Directory/topGoAnalysis.R
@@ -7,6 +7,8 @@ library("topGO")
 #filter DESeq for significance
 sig <- na.omit(res)
 sig <- sig[sig$padj<0.05,]
+sigdf <- as.data.frame(sig)
+sigdf <- tibble::rownames_to_column(sigdf, "gene_id")
 sig_up <- as.data.frame(sig[sig$log2FoldChange>=1,])
 sig_up <- tibble::rownames_to_column(sig_up, "gene_id")
 sig_down <- as.data.frame(sig[sig$log2FoldChange<=1,])
@@ -72,50 +74,110 @@ up_GO_filtered <- up_summary %>%
   filter(weight01 <= 0.05) %>%
   head(n = 20)
 
-up_GO_filtered %>% 
-  ggplot(aes(x = Term, y = GeneRatio)) +
-  geom_col(width = 0.05) + 
-  geom_point(size = 3) +
-  coord_flip() # Flip the axes so the x-axis labels are readable
-
-down_GO_filtered <- down_summary %>%
-  mutate(GeneRatio = Significant/Annotated, weight01 = as.numeric(weight01)) %>%
-  filter(weight01 <= 0.05) %>%
-  head(n = 20)
-
-down_GO_filtered %>% 
-  ggplot(aes(x = Term, y = GeneRatio)) +
-  geom_col(width = 0.05) + 
-  geom_point(size = 3) +
-  coord_flip() # Flip the axes so the x-axis labels are readable
-
-down_GO_filtered_arranged <- down_GO_filtered %>% 
+up_GO_filtered_arranged <- up_GO_filtered %>% 
   arrange(GeneRatio) %>%
   mutate(Term = factor(Term))
 
 # Now let's extract the order of the term column
-order_term <- down_GO_filtered_arranged %>% 
+up_order_term <- up_GO_filtered_arranged %>% 
   pull(Term) # pull() extracts a column as a vector
 
-
-down_GO_filtered_arranged %>% 
-  ggplot(aes(x= Term, y = GeneRatio, colour = weight01)) +
-  geom_col(width = 0.05) +
-  geom_point(size = 3) +
-  coord_flip() +
-  scale_x_discrete(limits = order_term) + 
-  scale_colour_gradient(low = "red", high = "blue")
-
-down_GO_filtered_arranged %>% 
+up_GO_filtered_arranged %>% 
   ggplot(aes(x= Term, y = GeneRatio, color = weight01)) +
+  ggtitle("Significant Upregulated GO Terms with Ontology MF") +
   geom_col(width = 0.05) +
   geom_point(aes(size = Significant)) + 
   coord_flip() +
-  scale_x_discrete(limits = order_term) +
+  scale_x_discrete(limits = up_order_term) +
   scale_color_gradient(low = "red", high = "blue") +
   
   # Add these to make our plot prettier
   theme_light() +
   labs(x = "GO Term Description", y = "Enrichment Ratio", color = "P-value", size = "Number of Significant Genes") + 
   theme(panel.border = element_rect(color = "black"), panel.grid = element_line(colour = "grey96")) +
-  scale_y_continuous(limits = c(0,1), breaks = seq(0, 1, 0.25), expand = c(0, 0)) # this changes the scale of the axes
+  scale_y_continuous(limits = c(0,1.1), breaks = seq(0, 1, 0.25), expand = c(0, 0)) # this changes the scale of the axes
+
+down_GO_filtered <- down_summary %>%
+  mutate(GeneRatio = Significant/Annotated, weight01 = as.numeric(weight01)) %>%
+  filter(weight01 <= 0.05) %>%
+  head(n = 20)
+
+down_GO_filtered_arranged <- down_GO_filtered %>% 
+  arrange(GeneRatio) %>%
+  mutate(Term = factor(Term))
+
+# Now let's extract the order of the term column
+down_order_term <- down_GO_filtered_arranged %>% 
+  pull(Term) # pull() extracts a column as a vector
+
+down_GO_filtered_arranged %>% 
+  ggplot(aes(x= Term, y = GeneRatio, color = weight01)) +
+  ggtitle("Significant Downregulated GO Terms with Ontology MF") +
+  geom_col(width = 0.05) +
+  geom_point(aes(size = Significant)) + 
+  coord_flip() +
+  scale_x_discrete(limits = down_order_term) +
+  scale_color_gradient(low = "red", high = "blue") +
+  
+  # Add these to make our plot prettier
+  theme_light() +
+  labs(x = "GO Term Description", y = "Enrichment Ratio", color = "P-value", size = "Number of Significant Genes") + 
+  theme(panel.border = element_rect(color = "black"), panel.grid = element_line(colour = "grey96")) +
+  scale_y_continuous(limits = c(0,1.1), breaks = seq(0, 1, 0.25), expand = c(0, 0)) # this changes the scale of the axes
+
+# Add labels to upregulated and downregulated dataframes
+up_GO <- up_summary %>% 
+  mutate(up_down = "UP")
+
+down_GO <- down_summary %>% 
+  mutate(up_down = "DOWN")
+
+# Make a joined dataframe
+joined_GO_filtered_arranged <- bind_rows(up_GO, down_GO) %>%
+  filter(weight01 <= 0.05) %>%
+  mutate(GeneRatio = Significant/Annotated, weight01 = as.numeric(weight01)) %>%
+  arrange(GeneRatio) %>%
+  mutate(Term = factor(Term)) %>%
+  head(n = 40)
+
+# Extract the column order
+order_term_joined <- joined_GO_filtered_arranged %>% 
+  pull(Term)
+
+joined_GO_filtered_arranged %>% 
+  ggplot(aes(x= Term, y = GeneRatio, color = weight01)) +
+  geom_point(aes(size= Significant)) +
+  coord_flip() +
+  scale_x_discrete(limits = order_term_joined) +
+  scale_color_gradient(low = "red", high = "blue") +
+  theme_light() +
+  labs(x = "GO Term Description", y = "Enrichment Ratio", color = "P-value", size = "Number of Significant Genes") +
+  theme(panel.border = element_rect(color = "black"), 
+        panel.grid = element_line(colour = "grey96"), 
+        strip.background = element_rect(colour = "black")) +
+  scale_y_continuous(limits = c(0, 1.1), breaks = seq(0, 1, 0.2), expand = c(0, 0)) +
+  facet_grid(.~ up_down)
+
+echlor_GOIDs <- read_tsv("echlor_GOIDs.tsv", col_names = c("gene_id", "GO_id"))
+GO_ID <- down_GO_filtered_arranged$GO.ID[2]
+filtered_GO_ID <- echlor_GOIDs %>%
+  filter(grepl(pattern = GO_ID, GO_id))
+
+gene_id_vector <- filtered_GO_ID %>% 
+  pull(gene_id)
+
+# Filter the data set
+dat_GO_filtered <- sigdf %>%
+  filter(gene_id %in% gene_id_vector) %>%
+  filter(padj <= 0.05 & log2FoldChange != 0)
+
+dat_GO_filtered %>% 
+  ggplot(aes(x = gene_id, y = log2FoldChange)) +
+  ggtitle("Genes Associated with GO:0004104 - Cholinesterase Activity") +
+  geom_errorbar(aes(ymin = log2FoldChange - lfcSE, ymax =log2FoldChange + lfcSE), width = 0.4) +
+  geom_bar(stat = "identity", color = "black", width = 0.8) +
+  coord_flip()+ 
+  labs(y = "log2(FC)", x= "Gene ID", fill = "Regulation") +
+  geom_hline(yintercept = 0) +
+  theme_light() +
+  theme(panel.border = element_rect(color = "black"), panel.grid = element_line(colour = "white"))
